ca90eb6e9677525c1fb3084c21aea3f2cd502427
aizernere <aizernerevangelista@gmail.com>
2024-12-05 23:09:49 +0800

    Completed

--- BackPropNeuralNet/Form1.Designer.cs
+++ BackPropNeuralNet/Form1.Designer.cs
@@ -132,6 +132,7 @@
             this.button4.TabIndex = 11;
             this.button4.Text = "Set 1";
             this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
             // 
             // button5
             // 
@@ -141,6 +142,7 @@
             this.button5.TabIndex = 12;
             this.button5.Text = "Set 2";
             this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
             // 
             // button6
             // 
@@ -150,6 +152,7 @@
             this.button6.TabIndex = 13;
             this.button6.Text = "Set 3";
             this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
             // 
             // button7
             // 
@@ -159,6 +162,7 @@
             this.button7.TabIndex = 14;
             this.button7.Text = "Set 4";
             this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
             // 
             // button8
             // 
@@ -168,6 +172,7 @@
             this.button8.TabIndex = 15;
             this.button8.Text = "Set 5";
             this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
             // 
             // button9
             // 
@@ -177,6 +182,7 @@
             this.button9.TabIndex = 16;
             this.button9.Text = "Set 6";
             this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
             // 
             // button10
             // 
@@ -186,6 +192,7 @@
             this.button10.TabIndex = 17;
             this.button10.Text = "Set 7";
             this.button10.UseVisualStyleBackColor = true;
+            this.button10.Click += new System.EventHandler(this.button10_Click);
             // 
             // button11
             // 
@@ -195,6 +202,7 @@
             this.button11.TabIndex = 18;
             this.button11.Text = "Set 8";
             this.button11.UseVisualStyleBackColor = true;
+            this.button11.Click += new System.EventHandler(this.button11_Click);
             // 
             // button12
             // 
@@ -204,6 +212,7 @@
             this.button12.TabIndex = 19;
             this.button12.Text = "Set 9";
             this.button12.UseVisualStyleBackColor = true;
+            this.button12.Click += new System.EventHandler(this.button12_Click);
             // 
             // button13
             // 
@@ -213,6 +222,7 @@
             this.button13.TabIndex = 20;
             this.button13.Text = "Set 10";
             this.button13.UseVisualStyleBackColor = true;
+            this.button13.Click += new System.EventHandler(this.button13_Click);
             // 
             // button14
             // 
@@ -222,6 +232,7 @@
             this.button14.TabIndex = 21;
             this.button14.Text = "Set 11";
             this.button14.UseVisualStyleBackColor = true;
+            this.button14.Click += new System.EventHandler(this.button14_Click);
             // 
             // button15
             // 
@@ -231,6 +242,7 @@
             this.button15.TabIndex = 22;
             this.button15.Text = "Set 12";
             this.button15.UseVisualStyleBackColor = true;
+            this.button15.Click += new System.EventHandler(this.button15_Click);
             // 
             // button16
             // 
@@ -240,6 +252,7 @@
             this.button16.TabIndex = 23;
             this.button16.Text = "Set 13";
             this.button16.UseVisualStyleBackColor = true;
+            this.button16.Click += new System.EventHandler(this.button16_Click);
             // 
             // button17
             // 
@@ -249,6 +262,7 @@
             this.button17.TabIndex = 24;
             this.button17.Text = "Set 14";
             this.button17.UseVisualStyleBackColor = true;
+            this.button17.Click += new System.EventHandler(this.button17_Click);
             // 
             // button18
             // 
@@ -258,6 +272,7 @@
             this.button18.TabIndex = 25;
             this.button18.Text = "Set 15";
             this.button18.UseVisualStyleBackColor = true;
+            this.button18.Click += new System.EventHandler(this.button18_Click);
             // 
             // button19
             // 
@@ -267,6 +282,7 @@
             this.button19.TabIndex = 26;
             this.button19.Text = "Set 16";
             this.button19.UseVisualStyleBackColor = true;
+            this.button19.Click += new System.EventHandler(this.button19_Click);
             // 
             // Form1
             // 
--- BackPropNeuralNet/Form1.cs
+++ BackPropNeuralNet/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Windows.Forms.VisualStyles;
 using Backprop;
 
 namespace BackPropNeuralNet
@@ -14,6 +15,25 @@ namespace BackPropNeuralNet
     public partial class Form1 : Form
     {
         NeuralNet nn;
+        int[,] inputSets = new int[,]
+            {
+                {0, 0, 0, 0},
+                {0, 0, 0, 1},
+                {0, 0, 1, 0},
+                {0, 0, 1, 1},
+                {0, 1, 0, 0},
+                {0, 1, 0, 1},
+                {0, 1, 1, 0},
+                {0, 1, 1, 1},
+                {1, 0, 0, 0},
+                {1, 0, 0, 1},
+                {1, 0, 1, 0},
+                {1, 0, 1, 1},
+                {1, 1, 0, 0},
+                {1, 1, 0, 1},
+                {1, 1, 1, 0},
+                {1, 1, 1, 1}
+            };
         public Form1()
         {
             InitializeComponent();
@@ -71,5 +91,134 @@ namespace BackPropNeuralNet
             nn.run();
             textBox5.Text = "" + nn.getOuputData(0);
         }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "" + inputSets[0, 0];
+            textBox2.Text = "" + inputSets[0, 1];
+            textBox3.Text = "" + inputSets[0, 2];
+            textBox4.Text = "" + inputSets[0, 3];
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "" + inputSets[1, 0];
+            textBox2.Text = "" + inputSets[1, 1];
+            textBox3.Text = "" + inputSets[1, 2];
+            textBox4.Text = "" + inputSets[1, 3];
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "" + inputSets[2, 0];
+            textBox2.Text = "" + inputSets[2, 1];
+            textBox3.Text = "" + inputSets[2, 2];
+            textBox4.Text = "" + inputSets[2, 3];
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "" + inputSets[3, 0];
+            textBox2.Text = "" + inputSets[3, 1];
+            textBox3.Text = "" + inputSets[3, 2];
+            textBox4.Text = "" + inputSets[3, 3];
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "" + inputSets[4, 0];
+            textBox2.Text = "" + inputSets[4, 1];
+            textBox3.Text = "" + inputSets[4, 2];
+            textBox4.Text = "" + inputSets[4, 3];
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "" + inputSets[5, 0];
+            textBox2.Text = "" + inputSets[5, 1];
+            textBox3.Text = "" + inputSets[5, 2];
+            textBox4.Text = "" + inputSets[5, 3];
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "" + inputSets[6, 0];
+            textBox2.Text = "" + inputSets[6, 1];
+            textBox3.Text = "" + inputSets[6, 2];
+            textBox4.Text = "" + inputSets[6, 3];
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "" + inputSets[7, 0];
+            textBox2.Text = "" + inputSets[7, 1];
+            textBox3.Text = "" + inputSets[7, 2];
+            textBox4.Text = "" + inputSets[7, 3];
+        }
+
+        private void button12_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "" + inputSets[8, 0];
+            textBox2.Text = "" + inputSets[8, 1];
+            textBox3.Text = "" + inputSets[8, 2];
+            textBox4.Text = "" + inputSets[8, 3];
+        }
+
+        private void button13_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "" + inputSets[9, 0];
+            textBox2.Text = "" + inputSets[9, 1];
+            textBox3.Text = "" + inputSets[9, 2];
+            textBox4.Text = "" + inputSets[9, 3];
+        }
+
+        private void button14_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "" + inputSets[10, 0];
+            textBox2.Text = "" + inputSets[10, 1];
+            textBox3.Text = "" + inputSets[10, 2];
+            textBox4.Text = "" + inputSets[10, 3];
+        }
+
+        private void button15_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "" + inputSets[11, 0];
+            textBox2.Text = "" + inputSets[11, 1];
+            textBox3.Text = "" + inputSets[11, 2];
+            textBox4.Text = "" + inputSets[11, 3];
+        }
+
+        private void button16_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "" + inputSets[12, 0];
+            textBox2.Text = "" + inputSets[12, 1];
+            textBox3.Text = "" + inputSets[12, 2];
+            textBox4.Text = "" + inputSets[12, 3];
+        }
+
+        private void button17_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "" + inputSets[13, 0];
+            textBox2.Text = "" + inputSets[13, 1];
+            textBox3.Text = "" + inputSets[13, 2];
+            textBox4.Text = "" + inputSets[13, 3];
+        }
+
+        private void button18_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "" + inputSets[14, 0];
+            textBox2.Text = "" + inputSets[14, 1];
+            textBox3.Text = "" + inputSets[14, 2];
+            textBox4.Text = "" + inputSets[14, 3];
+        }
+
+        private void button19_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "" + inputSets[15, 0];
+            textBox2.Text = "" + inputSets[15, 1];
+            textBox3.Text = "" + inputSets[15, 2];
+            textBox4.Text = "" + inputSets[15, 3];
+        }
+
     }
 }
